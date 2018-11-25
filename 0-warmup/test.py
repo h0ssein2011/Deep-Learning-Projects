ee18e8421e8b654d5847cbee2e66760328f6d0b4
h0ssen2011 <h0ssein2011@yahoo.com>
2018-11-25 14:17:15 +0330

    These are couresea sepecalization excersies for personal records

--- 0-warmup/test.py
+++ 0-warmup/test.py
@@ -4,7 +4,7 @@ def Relu(z):
 
 # import pandas as pd
 # import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
 x=list(range(-20 , 20))
@@ -12,5 +12,5 @@ y =[Relu(item) for item in x]
 
 print(x[:5])
 print(y[:5])
-# plt.plot(x,y)
-# plt.show()
+plt.plot(x,y)
+plt.show()
